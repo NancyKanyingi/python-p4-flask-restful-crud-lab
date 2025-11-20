b3de4bef23e1f4cfeedfd060dc57478135557207
Nancy Wangui Kanyingi <wangui.kanyingi2015@gmail.com>
2025-11-21 01:55:00 +0300

    Initial commit

--- server/app.py
+++ server/app.py
@@ -20,8 +20,8 @@ api = Api(app)
 class Plants(Resource):
 
     def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
+        plant = [plant.to_dict() for plant in Plant.query.all()]
+        return make_response(jsonify(plant), 200)
 
     def post(self):
         data = request.get_json()
@@ -38,18 +38,40 @@ class Plants(Resource):
         return make_response(new_plant.to_dict(), 201)
 
 
-api.add_resource(Plants, '/plants')
-
 
 class PlantByID(Resource):
 
     def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return {"error": "Plant not found"}, 404
+        return make_response(jsonify(plant.to_dict()), 200)
 
+
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return {"error": "Plant not found"}, 404
+
+        data =request.get_json()
+        for attr, value in data.items():
+            setattr (plant, attr, value)
+        
+        db.session.commit()
+        
+        return make_response(plant.to_dict(), 200)
+
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return {"error": "Plant not found"}, 404
+        db.session.delete(plant)
+        db.session.commit()
+
+        return make_response({"message": "Plant deleted"}, 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
